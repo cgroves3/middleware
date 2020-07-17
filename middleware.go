c5b75c59a94584038bfcd7747e011fc4b217a552
Craig Groves <cgroves06@gmail.com>
2020-07-17 11:26:18 -0400

    Removed commented code.

--- middleware.go
+++ middleware.go
@@ -4,13 +4,7 @@ import (
 	"net/http"
 )
 
-func Nest(handlers []http.HandlerFunc) http.HandlerFunc {
-	//var result http.HandlerFunc = nil
-	//for i := len(handlers) - 1; i > 0; i-- {
-	//	if i < len(handlers) - 1 {
-	//		result = handlers[i]
-	//	}
-	//}
+func Join(handlers []http.HandlerFunc) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		for _, handler := range handlers {
 			handler.ServeHTTP(writer, request)
