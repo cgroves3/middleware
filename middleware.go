18e1096ee9699862a5f403a219001355bc526a5a
Craig Groves <cgroves06@gmail.com>
2020-07-17 12:36:50 -0400

    Updated to be variadic function.

--- middleware.go
+++ middleware.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-func Join(handlers []http.HandlerFunc) http.HandlerFunc {
+func Join(handlers ...http.HandlerFunc) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		for _, handler := range handlers {
 			handler.ServeHTTP(writer, request)
